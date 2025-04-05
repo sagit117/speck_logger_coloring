6afd78e0d8c65e03854b6e782eaed66b0c61feb6
axel <sagit117@gmail.com>
2025-04-06 00:31:29 +0300

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![crate_type = "lib"]
 #![crate_name = "speck_logger_coloring"]
+
 use chrono::Local;
-use speck_logger::{LogLevel, Logger};
+pub use speck_logger::{Logger, LogLevel, LogWriter};
 use speck_text_coloring::{black, blue, purple, red, yellow};
 
 /// Функционал для создания цветного консольного логгера.
@@ -20,18 +21,18 @@ pub struct ColorizedConsoleLogger {
 }
 
 impl ColorizedConsoleLogger {
-    pub fn new(level: Level) -> Self {
+    pub fn new(level: LogLevel) -> Self {
         ColorizedConsoleLogger {
             logger: Logger {
                 out: Box::new(std::io::stdout()),
-                level: level_to_log_level(level),
+                level,
                 formatter: |message: &str, level: LogLevel| -> String { format!("{} [{: <16}] {}\n", date_format_now(), color_level(level), message) }
             }
         }
     }
 }
 
-impl LogWriter for ColorizedConsoleLogger {
+impl LogWriter<()> for ColorizedConsoleLogger {
     fn info(&mut self, message: &str) {
         self.logger.info(message).expect("Ошибка записи лога!");
     }
@@ -49,43 +50,6 @@ impl LogWriter for ColorizedConsoleLogger {
     }
 }
 
-pub enum Level {
-    ALL,
-    DEBUG,
-    INFO,
-    WARNING,
-    ERROR
-}
-
-impl Level {
-    /// Метод воссоздаст Level из строки или вернет Level::ALL, если операция не будет успешной.
-    pub fn from_string(str: &str) -> Level {
-        let level = str.to_uppercase();
-
-        if level.eq("DEBUG") {
-            Level::DEBUG
-        } else if level.eq("ONFO") {
-            Level::INFO
-        } else if level.eq("WARNING") {
-            Level::WARNING
-        } else if level.eq("ERROR") {
-            Level::ERROR
-        } else {
-            Level::ALL
-        }
-    }
-}
-
-fn level_to_log_level(level: Level) -> LogLevel {
-    match level {
-        Level::ALL => LogLevel::ALL,
-        Level::DEBUG => LogLevel::DEBUG,
-        Level::INFO => LogLevel::INFO,
-        Level::WARNING => LogLevel::WARNING,
-        Level::ERROR => LogLevel::ERROR,
-    }
-}
-
 fn color_level(level: LogLevel) -> String {
     match  level {
         LogLevel::ALL => black(&level.to_string()),
@@ -99,11 +63,4 @@ fn color_level(level: LogLevel) -> String {
 fn date_format_now() -> String {
     let now = Local::now();
     now.format("%Y-%m-%d %H:%M:%S").to_string()
-}
-
-pub trait LogWriter {
-    fn info(&mut self, message: &str);
-    fn warning(&mut self, message: &str);
-    fn debug(&mut self, message: &str);
-    fn error(&mut self, message: &str);
 }
